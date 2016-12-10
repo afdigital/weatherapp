fa3b1f2903ef31fe6b8dfbd962ccbfe745cc1a7a
Satchit <pixelsandapps@gmail.com>
2016-12-10 21:58:00 +0400

    Added foundation and jquery

--- app/app.jsx
+++ app/app.jsx
@@ -6,6 +6,10 @@ var Weather = require('Weather');
 var About = require('About');
 var Examples = require('Examples');
 
+// Load foundation
+require('style!css!foundation-sites/dist/css/foundation.min.css');
+$(document).foundation();
+
 // ES6 destructuring syntax used instead of the following:
 // var Route = require('react-router').Route;
 // var Router = require('react-router').Router;
